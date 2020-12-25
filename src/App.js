33cdbf010a2d7cd966fed100c0f516b649803e33
ankit verma <av4135850.19je0144@cse.iitism.ac.in>
2020-12-25 11:42:06 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import Address from './components/pages/Address';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        
+
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/gallery' component={Address} />
+          <Route path='/sign-up' component={SignUp} />
+          
+          
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
